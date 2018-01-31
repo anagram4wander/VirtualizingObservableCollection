8e0f3e958314f54b76ab6fbc3c4d22050fbced67
Jan Pluskal <ipluskal@fit.vutbr.cz>
2018-01-31 07:28:40 +0100

    Add tests template.

---
+++ VirtualizingCollection.Tests/TestItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+
+namespace VirtualizingCollection.Tests
+{
+    public class TestItem
+    {
+        private static int _root = -1;
+        public int Index { get; set; } = Interlocked.Increment(ref _root);
+    }
+}
